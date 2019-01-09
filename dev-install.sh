79bb131e5a4f530ac7a5452e1c7670d8e55e5b97
Drew Immerman <drewimmerman@gmail.com>
2019-01-08 21:03:12 -0500

    [Issue #4] Update VS Code

--- dev-install.sh
+++ dev-install.sh
@@ -17,10 +17,8 @@ dnf install vim -y
 
 # VS Code
 echo "Installing VS Code..."
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.\
-    microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://\
-    packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update
 dnf install code -y
 
