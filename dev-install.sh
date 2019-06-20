d425e9375725de54953a79903fee9b3a12275d17
Drew Immerman <drewimmerman@gmail.com>
2019-06-20 13:16:36 -0400

    Removed httpd/server utilities

--- dev-install.sh
+++ dev-install.sh
@@ -31,18 +31,6 @@ echo "Installing NodeJS";
 dnf install -y npm
 
 ##
-# Code utilities
-#
-
-# Doxygen
-echo "Installing Doxygen...";
-dnf install -y doxygen
-
-# Sass
-echo "Installing Dart-Sass";
-dnf install -y rubygem-sass
-
-##
 # File
 #
 
@@ -65,74 +53,6 @@ gpgcheck=1
 gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF
 dnf install google-chrome-stable -y
-
-# Lamp
-echo "Installing Lamp Stack..."
-dnf install -y httpd php mariadb mariadb-server
-dnf install -y phpmyadmin
-mkdir -p /etc/httpd/sites-enabled
-
-cat <<EOT >> /etc/httpd/conf/httpd.conf
-IncludeOptional /etc/httpd/sites-enabled/*.conf
-EOT
-
-# Lamp firewall
-echo "Configuring Firewall..."
-firewall-cmd --permanent --add-service=http
-firewall-cmd --permanent --add-service=https
-firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --permanent --add-port=443/tcp
-firewall-cmd --reload
-
-# Enabling httpd
-echo "Enabling httpd..."
-systemctl restart httpd
-systemctl enable httpd
-
-# SELinux
-echo "SELinux - Setting httpd network connections"
-setsebool -P httpd_can_network_connect on
-
-# Lamp permissions
-echo "Setting permissions..."
-rm -rf /var/www/permissions
-
-cat <<EOT >> /var/www/permissions
-#!/bin/bash
-chown \$1:apache -R /var/www/\$2;
-
-find . -type f -exec chmod 0644 {} \;
-find . -type d -exec chmod 0755 {} \;
-
-chcon -t httpd_sys_content_t /var/www/\$2 -R;
-
-chmod 755 /var/www/permissions;
-EOT
-
-chmod 755 /var/www/permissions;
-
-# MariaDB
-echo "Configuring MariaDB..."
-systemctl start mariadb.service
-systemctl enable mariadb.service
-
-firewall-cmd --add-service=mysql
-/usr/bin/mysql_secure_installation
-
-# Xdebug
-echo "Installing xdebug..."
-dnf install -y php-xdebug
-
-cat <<EOT >> /etc/php.ini
-
-[XDebug]
-xdebug.remote_enable = 1
-xdebug.remote_autostart = 1
-EOT
-
-# PHPUnit
-echo "Installing PHPUnit...";
-dnf install -y phpunit
 
 # Postman
 echo "Installing Postman";
@@ -161,49 +81,6 @@ Terminal=false
 Type=Application
 Categories=Development;
 EOL
-
-##
-# Server administration
-#
-
-# OpenXenManager
-echo "Installing OpenXenManager";
-yum install -y pygtk2 gtk-vnc-python rrdtool
-dnf install -y pygtk2-libglade gtk-vnc-python
-pip install -y configobj
-
-cd /tmp
-git clone https://github.com/OpenXenManager/openxenmanager.git
-cd openxenmanager
-python setup.py install
-
-mkdir -p /opt/openxenmanager
-cp /mnt/myfiles/Scripts/bin/xencenter.png /opt/openxenmanager/icon.png
-
-cat > /home/dimmerman/.local/share/applications/openxenmanager.desktop <<EOL
-[Desktop Entry]
-Encoding=UTF-8
-Name=XenManager
-Exec=openxenmanager
-Icon=/opt/openxenmanager/icon.png
-Terminal=false
-Type=Application
-Categories=Development;
-EOL
-
-##
-# Theme
-#
-
-# Arc theme
-dnf install -y arc-theme
-
-# Paper theme (icons etc)
-dnf config-manager --add-repo https://download.opensuse.org/repositories/home:snwh:paper/Fedora_25/home:snwh:paper.repo
-dnf install -y paper-icon-theme
-dnf config-manager --add-repo https://download.opensuse.org/repositories/home:snwh:paper/Fedora_25/home:snwh:paper.repo
-dnf install -y paper-gtk-theme
-dnf install -y gnome-tweak-tool
 
 ##
 # Cleanup
